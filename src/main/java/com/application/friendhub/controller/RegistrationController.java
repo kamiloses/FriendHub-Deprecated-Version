afdd1ca996876a1b01dc9340b143cdad90b2e33a
kamiloses <kamil.kurzaj04@gmail.com>
2024-04-23 10:40:57 +0200

    Added SecondStep endpoints with  business logic(sending emails, saving to dataBase after writing correct token)

--- src/main/java/com/application/friendhub/controller/RegistrationController.java
+++ src/main/java/com/application/friendhub/controller/RegistrationController.java
@@ -1,9 +1,11 @@
 package com.application.friendhub.controller;
 
+import com.application.friendhub.EmailSender.EmailServiceImpl;
 import com.application.friendhub.Entity.UserDetailsEntity;
 import com.application.friendhub.Entity.UserEntity;
 import com.application.friendhub.Repository.UserDetailsRepository;
 import com.application.friendhub.Repository.UserRepository;
+import com.application.friendhub.dto.SecondStepDto;
 import com.application.friendhub.registrationProcess.FirstStepDto;
 import com.application.friendhub.fronted.SelectOptionService;
 import com.application.friendhub.registrationProcess.FirstStepDtoService;
@@ -18,6 +20,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
+
 @Controller
 public class RegistrationController {
 
@@ -27,12 +31,16 @@ public class RegistrationController {
     private final FirstStepDtoService firstStepDtoService;
     private final UserDetailsRepository userDetailsRepository;
     private final UserRepository userRepository;
-
-    public RegistrationController(SelectOptionService selectOptionService, FirstStepDtoService firstStepDtoService, UserDetailsRepository userDetailsRepository, UserRepository userRepository) {
+    private final EmailServiceImpl emailService;
+    private String token;
+    private UserEntity user;
+    private UserDetailsEntity userDetailsEntity;
+    public RegistrationController(SelectOptionService selectOptionService, FirstStepDtoService firstStepDtoService, UserDetailsRepository userDetailsRepository, UserRepository userRepository, EmailServiceImpl emailService) {
         this.selectOptionService = selectOptionService;
         this.firstStepDtoService = firstStepDtoService;
         this.userDetailsRepository = userDetailsRepository;
         this.userRepository = userRepository;
+        this.emailService = emailService;
     }
 
     @GetMapping("/register/firstStep")
@@ -54,7 +62,7 @@ public class RegistrationController {
                             @RequestParam("dob-day") String day,
                             @RequestParam("dob-month") String month,
                             @RequestParam("dob-year") String year
-    ) {
+    ) throws IOException {
 
         if (bindingResult.hasErrors()) {
             model.addAttribute("days", selectOptionService.getDays());
@@ -71,22 +79,40 @@ public class RegistrationController {
             //todo dodać komunikat związany z  validateDayOfMonth
             return "html/registrationAndLogin/firstStep";
         }
-        UserEntity user = firstStepDtoService.convertFirstStepDtoToUserEntity(firstStepDto);
-        UserEntity userEntity = userRepository.save(user);
-        UserDetailsEntity userDetailsEntity = firstStepDtoService.convertFirstStepDtoToUserDetailsEntity(firstStepDto,userEntity);
-        userDetailsRepository.save(userDetailsEntity);
+
+        user = firstStepDtoService.convertFirstStepDtoToUserEntity(firstStepDto);
+
+        userDetailsEntity = firstStepDtoService.convertFirstStepDtoToUserDetailsEntity(firstStepDto, user);
 
 
-        return "redirect:/register/friendHub/secondStep";
+        token = emailService.generateToken();
+        LOG.error(token);
+        emailService.sendEmail(user.getEmail(),token);
+
+        return "redirect:/register/secondStep";
     }
 
-
-    @GetMapping("/register/friendHub/secondStep")
-    public String secondStep() {
-
+    @GetMapping("/register/secondStep")
+     public String secondStep(Model model) {
+     model.addAttribute("secondStepDto",new SecondStepDto());
 
         return "html/registrationAndLogin/SecondStep";
     }
+    @PostMapping("/register/secondStep/add")
+    public String secondStep(@ModelAttribute("secondStepDto") SecondStepDto secondStepDto,
+                             BindingResult bindingResult) {
+        if (bindingResult.hasErrors() | !secondStepDto.getToken().equals(token.toUpperCase())) {
+
+            return "html/registrationAndLogin/SecondStep";
+        }
+
+        userRepository.save(user);
+        userDetailsRepository.save(userDetailsEntity);
+
+        return  "redirect:/friendHub/login";
+    }
+
+
 
 
     @GetMapping("/friendHub/login")
