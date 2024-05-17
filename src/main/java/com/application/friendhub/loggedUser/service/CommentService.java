a7a63e25145cec318dbaffde2ce599db52907143
kamiloses <kamil.kurzaj04@gmail.com>
2024-05-17 07:18:36 +0200

    added commentDtoToEntity method

--- src/main/java/com/application/friendhub/loggedUser/service/CommentService.java
+++ src/main/java/com/application/friendhub/loggedUser/service/CommentService.java
@@ -1,25 +1,43 @@
 package com.application.friendhub.loggedUser.service;
 
 import com.application.friendhub.Entity.CommentsEntity;
+import com.application.friendhub.Entity.TimelineEntity;
+import com.application.friendhub.Entity.UserEntity;
+import com.application.friendhub.Repository.TimelineRepository;
+import com.application.friendhub.Repository.UserRepository;
 import com.application.friendhub.loggedUser.dto.CommentDto;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class CommentService {
 
 
-    CommentDto commentEntityToDto(CommentsEntity commentsEntity) {
+   private UserRepository userRepository;
+   private TimelineRepository timelineRepository;
+
+    public CommentService(UserRepository userRepository, TimelineRepository timelineRepository) {
+        this.userRepository = userRepository;
+        this.timelineRepository = timelineRepository;
+    }
+
+    public CommentsEntity commentDtoToEntity(CommentDto commentDto) {
         String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        UserEntity user = userRepository.findUserEntityByEmail(name).orElseThrow(() -> new RuntimeException("User Not Found"));
+        TimelineEntity timelineEntity = timelineRepository.findById(commentDto.getTimelineId()).orElseThrow(() -> new RuntimeException("Timeline Not Found"));
+        CommentsEntity commentsEntity = new CommentsEntity();
+        commentsEntity.setFirstName(user.getUserDetailsEntity().getFirstName());
+        commentsEntity.setLastName(user.getUserDetailsEntity().getLastName());
+        commentsEntity.setComment(commentDto.getComment());
+       commentsEntity.setDateOfPublication(new Date());
+       commentsEntity.setCommentsEntity(timelineEntity); //todo ustaw
 
-        CommentDto commentDto = new CommentDto();
-        commentDto.setFirstName(commentsEntity.getFirstName());
-        commentDto.setLastName(commentsEntity.getLastName());
-        commentDto.setComment(commentsEntity.getComment());
-        commentDto.setDateOfPublication(commentsEntity.getDateOfPublication());
-        /*commentDto.setTimelineId(1L);*/
+       return commentsEntity;
+    }
 
-    return commentDto;}
+
 
 
 
