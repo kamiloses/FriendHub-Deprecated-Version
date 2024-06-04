c11a412e85c85804a86e29315589aba7fc733dc2
kamiloses <kamil.kurzaj04@gmail.com>
2024-06-04 15:50:08 +0200

    added option that disallow you from searching your own account

--- src/main/java/com/application/friendhub/loggedUser/service/FriendsService.java
+++ src/main/java/com/application/friendhub/loggedUser/service/FriendsService.java
@@ -1,7 +1,10 @@
 package com.application.friendhub.loggedUser.service;
 
 import com.application.friendhub.Entity.UserDetailsEntity;
+import com.application.friendhub.Entity.UserEntity;
 import com.application.friendhub.Repository.UserDetailsRepository;
+import com.application.friendhub.Repository.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,14 +14,20 @@ import java.util.List;
 @Service
 public class FriendsService {
 
+    private final UserRepository userRepository;
     UserDetailsRepository userDetailsRepository;
 
-    public FriendsService(UserDetailsRepository userDetailsRepository) {
+    public FriendsService(UserDetailsRepository userDetailsRepository, UserRepository userRepository) {
         this.userDetailsRepository = userDetailsRepository;
+        this.userRepository = userRepository;
     }
 
 
     public List<UserDetailsEntity> findUserByNameOrSurname( @RequestParam(required = false) String fullName) {
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        UserEntity user = userRepository.findUserEntityByEmail(name).orElseThrow();
+
+
         List<UserDetailsEntity> foundUsers=null;
         String firstName;
         String lastName;
@@ -26,14 +35,14 @@ public class FriendsService {
         if (parts.length == 2) {
             firstName = parts[0];
             lastName = parts[1];
-             foundUsers = userDetailsRepository.findUserDetailsEntitiesByFirstNameOrLastName(firstName, lastName);
+             foundUsers = userDetailsRepository.findUserDetailsEntitiesByFirstNameOrLastName(firstName, lastName,user.getId());
             if (foundUsers.isEmpty()) {
                 firstName = parts[1];
                 lastName = parts[0];
-                foundUsers = userDetailsRepository.findUserDetailsEntitiesByFirstNameOrLastName(firstName, lastName);
+                foundUsers = userDetailsRepository.findUserDetailsEntitiesByFirstNameOrLastName(firstName, lastName,user.getId());
             }
         } else {
-             foundUsers = userDetailsRepository.findUserDetailsEntitiesByFirstNameOrLastName(fullName, fullName);
+             foundUsers = userDetailsRepository.findUserDetailsEntitiesByFirstNameOrLastName(fullName, fullName,user.getId());
 
 
         }
