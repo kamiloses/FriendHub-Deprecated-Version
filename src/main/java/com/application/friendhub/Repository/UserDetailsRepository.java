75b1c33ae223c2e3741eabe429af6a64191e5e0c
kamiloses <kamil.kurzaj04@gmail.com>
2024-06-04 15:50:45 +0200

    added adding friend option

--- src/main/java/com/application/friendhub/Repository/UserDetailsRepository.java
+++ src/main/java/com/application/friendhub/Repository/UserDetailsRepository.java
@@ -2,6 +2,8 @@ package com.application.friendhub.Repository;
 
 import com.application.friendhub.Entity.UserDetailsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,7 +11,11 @@ public interface UserDetailsRepository extends JpaRepository<UserDetailsEntity,L
 
      UserDetailsEntity findUserDetailsEntityByUserEntity_Email(String email);
 
-     List<UserDetailsEntity> findUserDetailsEntitiesByFirstNameOrLastName(String firstName,String lastName);
+
+     @Query("SELECT u FROM UserDetailsEntity u WHERE (u.firstName = :firstName OR u.lastName = :lastName) AND u.userEntity.id != :userId")
+     List<UserDetailsEntity> findUserDetailsEntitiesByFirstNameOrLastName( @Param("firstName") String firstName,
+                                                                           @Param("lastName") String lastName,
+                                                                           @Param("userId") Long userId);
 
 
 
